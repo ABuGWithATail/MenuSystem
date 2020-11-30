bd28bf4e5ecb1e72c29272cb05b793ed8518f2b2
ABuGWithATail <joshuaascott7@gmail.com>
2020-11-30 12:56:48 +1100

    Menu Systems Time
    initial Commit

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject titleScreen;
+    public GameObject mainMenuObject;
+    public GameObject settingsMenu;
+
+    //this sscript is my original script from the first assignment, but I have lost the project and got the script, So I'm going to need to remake this script in a new project to get it to work again.
+    public void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            Debug.Log("Escape Pressed");
+            if (titleScreen.activeInHierarchy == false)
+            {
+                Debug.Log("Hierarchy False");
+                titleScreen.SetActive(true);
+                mainMenuObject.SetActive(false);
+                settingsMenu.SetActive(false);
+            }
+        }
+    }
+
+    private void OnGUI()
+    {
+        Event e = Event.current;
+        if (e.isKey)
+        {
+            if (titleScreen.activeInHierarchy == true)
+            {
+                titleScreen.SetActive(false);
+                mainMenuObject.SetActive(true);
+                settingsMenu.SetActive(false);
+            }
+        }
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Exiting Game.");
+        Application.Quit();
+    }
+
+    public void GoToGame()
+    {
+        Debug.Log("See you in the game.");
+        SceneManager.LoadScene("GameScene");
+    }
+
+
+}
---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+
+public class SettingsMenu : MonoBehaviour
+{
+    public GameObject settingsMenu;
+    public Resolution[] resolutions;
+    public Dropdown resolution;
+    private Scene currentScene;
+    public AudioMixer masterAudio;
+
+    private void Start()
+    {
+        currentScene = SceneManager.GetActiveScene();
+        Time.timeScale = 1;
+
+        resolutions = Screen.resolutions;
+        resolution.ClearOptions();
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+            resolution.AddOptions(options);
+            resolution.value = currentResolutionIndex;
+            resolution.RefreshShownValue();
+        }
+    }
+
+    public void SetResolution(int _resolutionIndex)
+    {
+        Resolution res = resolutions[_resolutionIndex];
+        Screen.SetResolution(res.width, res.height, Screen.fullScreen);
+    }
+
+    public void SetFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+
+    public void Quality(int _qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(_qualityIndex);
+    }
+
+    public void ChangeMusicVolume(float volume)
+    {
+        masterAudio.SetFloat("MusicVolume", volume);
+
+    }
+    public void ChangeSFXVolume(float volume)
+    {
+        masterAudio.SetFloat("SFXVolume", volume);
+    }
+
+    public void ToggleMute(bool isMuted)
+    {
+        if (isMuted)
+        {
+            masterAudio.SetFloat("MusicVolume", -80);
+            masterAudio.SetFloat("SFXVolume", -80);
+        }
+        else
+        {
+            masterAudio.SetFloat("MusicVolume", 0);
+            masterAudio.SetFloat("SFXVolume", 0);
+        }
+    }
+
+    public void UnPause()
+    {
+        Time.timeScale = 1;
+        settingsMenu.SetActive(false);
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene("mainMenu");
+    }
+
+    private void Update()
+    {
+        string sceneName = currentScene.name;
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (settingsMenu.activeInHierarchy == false)
+            {
+                Time.timeScale = 1;
+                settingsMenu.SetActive(false);
+            }
+            if (sceneName == "GameScene")
+            {
+                Time.timeScale = 0;
+                settingsMenu.SetActive(true);
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+
+}
